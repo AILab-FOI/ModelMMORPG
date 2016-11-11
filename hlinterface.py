466bdaca01e64a622786addab2fabfb1321bc81c
Markus Schatten <markus.schatten@foi.hr>
2016-11-11 15:13:57 +0100

    Added initial agent methods to work with the reasoning engine.

--- hlinterface.py
+++ hlinterface.py
@@ -10,6 +10,47 @@ class ManaWorldPlayer( spade.Agent.BDIAgent, lli.Connection ):
 	def say( self, msg ):
 		print '%s: %s' % ( self.name.split( '@' )[ 0 ], str( msg ) )
 
+	def updateKB( self ):
+		''' Update the knowledgebase based on current observation
+		    of the environment in TMW, e.g.:
+			- list the inventory
+			- list current visible items lying around (include location and type)
+			- list visible mobs/NPCs/other players (include location and name)
+			- list any changes in quest acomplishment (e.g. quest solved)
+			- list any ongoing or done conversation with NPCs that are possibly
+			  giving out a quest
+			- any changes in social network (e.g. friends, enemies etc.)
+			- any changes in organization (e.g. party membership)
+			...? 
+		    and update the knowledge base accordingly'''
+		pass
+
+	def updateObjectives( self ):
+		''' List all possible objectives (e.g. unsolved quests) '''
+		self.quests = self.askBelieve( 'waiting_quest( NPC, a, Name ).' )
+		time.sleep( 1 )
+		if self.quests:
+			self.say( 'My current quests are:' )
+			for quest in self.quests:
+				try:
+					self.say( quest[ 'Name' ] + ' given by ' + quest[ 'NPC' ] )
+				except:
+					pass
+			return self.quests
+		else:
+			self.say( 'I have no current quests!' )
+			return [ { 'Name':'random_walk', 'NPC':'anonymous' } ]
+		
+
+	def selectObjective( self, objectives ):
+		''' Select most relevant objective (quest) to be solved next '''
+		quests = self.askBelieve( 'sort_quests( A ), quest_no( NPC, A, Name, No ).' )
+		if quests:
+			next = sorted( quests, key=lambda x: x[ 'No' ] )[ 0 ][ 'Name' ]
+			self.say( 'My next objective is quest: ' + next )
+			return next
+		time.sleep( 1 )
+
 	class Login( spade.Behaviour.OneShotBehaviour ):
 		def _process( self ):
 			self.myAgent.login()
@@ -21,19 +62,20 @@ class ManaWorldPlayer( spade.Agent.BDIAgent, lli.Connection ):
 
 	class Reason( spade.Behaviour.Behaviour ):
 		def _process( self ):
-			# update KB
-			# self.myAgent.kb.addSentence(
-			# generate possible objectives
-			# choose objective
+			self.myAgent.updateKB()
+			obj = self.myAgent.updateObjectives()
+			if obj[ 0 ][ 'Name' ] != 'random_walk':
+				next = self.myAgent.selectObjective( obj )
+			else:
+				next = 'random_walk'
+			
 			# plan
-			pass
 
 	class ChangeRole(spade.Behaviour.OneShotBehaviour):
 		"""Behaviour to change the Role of the Agent. The Agent will acquire behaviours of the needed Role."""
 		def _process(self):
 			pass
 			
-
 	def __init__( self, SERVER, PORT, USERNAME, PASSWORD, CHARACTER, *args, **kwargs ):
 		spade.Agent.Agent.__init__( self, *args, **kwargs )
 		lli.Connection.__init__( self, SERVER, PORT, USERNAME, PASSWORD, CHARACTER )
@@ -41,7 +83,7 @@ class ManaWorldPlayer( spade.Agent.BDIAgent, lli.Connection ):
 		
 		self.kb = KB()
 		try:		
-			self.kb.loadModule( 'planner.pl' )
+			self.kb.ask( "['planner.pl']" )
 			self.say( 'Planner loaded!' )
 		except:
 			self.say( 'Error while loading planner, aborting!' )
@@ -49,8 +91,8 @@ class ManaWorldPlayer( spade.Agent.BDIAgent, lli.Connection ):
 			sys.exit()
 
 		try:		
-			self.kb.loadModule( 'tmwmap.P' )
-			self.say( 'Map knowledge base loaded!' )
+			self.say( 'Map knowledge base loading in background!' )
+			self.kb.ask( "['tmwmap.P']" )
 		except:
 			self.say( 'Error while loading map knowledge base, aborting!' )
 			import sys
@@ -60,6 +102,9 @@ class ManaWorldPlayer( spade.Agent.BDIAgent, lli.Connection ):
 	def _setup( self ):
 		login = self.Login()
 		self.addBehaviour( login )
+
+		reason = self.Reason()
+		self.addBehaviour( reason )
 
 if __name__ == '__main__':
 	from testconf import *
